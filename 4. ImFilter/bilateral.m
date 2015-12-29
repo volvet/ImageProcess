ebefe3bdd973dec524596dc24b6bba00200d7b77
volvet <qizh@cisco.com>
2015-12-29 23:16:51 +0800

    bilateral filter for rgb image

--- 4. ImFilter/bilateral.m
+++ 4. ImFilter/bilateral.m
@@ -3,6 +3,14 @@
 %
 
 function out = bilateral(in, w, sigmaD, sigmaR)
+if size(in, 3) == 1 
+    out = bFltGray(in, w, sigmaD, sigmaR);
+else 
+    out = bFltColor(in, w, sigmaD, sigmaR);
+end
+
+
+function out = bFltGray(in, w, sigmaD, sigmaR)
 
 [X, Y] = meshgrid(-w:w, -w:w);
 G = exp(-(X.^2 + Y.^2)/(2*sigmaD^2));
@@ -23,4 +31,35 @@ for i = 1:dim(1)
     end
 end
 
-function out = bfltColor(in, w, sigmaD, sigmaR)
+function out = bFltColor(in, w, sigmaD, sigmaR)
+
+[X, Y] = meshgrid(-w:w, -w:w);
+G = exp(-(X.^2 + Y.^2)/(2*sigmaD^2));
+
+sigmaR = sigmaR * 3;
+
+dim = size(in);
+out = zeros(dim);
+for i = 1:dim(1)
+    for j = 1:dim(2)
+        iMin = max(i-w, 1);
+        iMax = min(i+w, dim(1));
+        jMin = max(j-2, 1);
+        jMax = min(j+w, dim(2));
+        I = in(iMin:iMax, jMin:jMax, :);
+        
+        dR = I(:,:,1) - in(i,j,1);
+        dG = I(:,:,2) - in(i,j,2);
+        dB = I(:,:,3) - in(i,j,3);
+        H = exp(-(dR.^2 + dG.^2 + dB.^2)/(2*sigmaR^2));
+        F = H.*G((iMin:iMax)-i+w+1,(jMin:jMax)-j+w+1);
+        norm = sum(F(:));
+        out(i,j,1) = sum(sum(F.*in(i,j,1)))/norm;
+        out(i,j,2) = sum(sum(F.*in(i,j,2)))/norm;
+        out(i,j,3) = sum(sum(F.*in(i,j,3)))/norm;
+    end
+end
+
+
+
+
