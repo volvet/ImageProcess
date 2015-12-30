3291946f9d5560c9ec4e0306946affbd8b732ebf
volvet <qizh@cisco.com>
2015-12-30 22:39:17 +0800

    use corr for sharpness filter

--- 4. ImFilter/sharping.m
+++ 4. ImFilter/sharping.m
@@ -4,7 +4,7 @@
 
 lena = imread('../pic/lena_color.png');
 H = fspecial('unsharp');
-sharpenedImg = imfilter(lena, H, 'replicate');
+sharpenedImg = imfilter(lena, H, 'corr', 'replicate');
 
 subplot(1, 2, 1), imshow(lena), title('original');
 subplot(1, 2, 2), imshow(sharpenedImg), title('sharpened');
