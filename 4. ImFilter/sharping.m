b34035d216e08cb96bb7af3ed7487ab6045a9d41
volvet <qizh@cisco.com>
2015-12-30 22:30:48 +0800

    sharping filter

---
+++ 4. ImFilter/sharping.m
@@ -0,0 +1,10 @@
+%
+%    Part 4  Sharpness Filter
+%
+
+lena = imread('../pic/lena_color.png');
+H = fspecial('unsharp');
+sharpenedImg = imfilter(lena, H, 'replicate');
+
+subplot(1, 2, 1), imshow(lena), title('original');
+subplot(1, 2, 2), imshow(sharpenedImg), title('sharpened');
