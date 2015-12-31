9a4bd6a8964c1ed91447d86d8392f0d25a30432c
volvet <qizh@cisco.com>
2015-12-31 16:52:59 +0800

    dilate

--- 5. Morphology/morphology.m
+++ 5. Morphology/morphology.m
@@ -4,7 +4,9 @@
 
 lena = imread('../pic/lena_color.png');
 SE = strel('diamond', 1);
-out = imerode(lena, SE);
+eroded = imerode(lena, SE);
+dilated = imdilate(lena, SE);
 
-subplot(1, 2, 1), imshow(lena), title('original');
-subplot(1, 2, 2), imshow(out), title('eroded');
+subplot(1, 3, 1), imshow(lena), title('original');
+subplot(1, 3, 2), imshow(out), title('eroded');
+subplot(1, 3, 3), imshow(dilated), title('dilated');
