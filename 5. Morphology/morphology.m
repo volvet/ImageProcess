f28e57686ea59215ddd6904a48d359a0d30bfea3
volvet <qizh@cisco.com>
2016-01-01 14:25:29 +0800

    edge detection

--- 5. Morphology/morphology.m
+++ 5. Morphology/morphology.m
@@ -8,5 +8,5 @@ eroded = imerode(lena, SE);
 dilated = imdilate(lena, SE);
 
 subplot(1, 3, 1), imshow(lena), title('original');
-subplot(1, 3, 2), imshow(out), title('eroded');
+subplot(1, 3, 2), imshow(eroded), title('eroded');
 subplot(1, 3, 3), imshow(dilated), title('dilated');
